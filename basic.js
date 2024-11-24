8407b791ee2d477d76ff0e8d92386af95de47c0a
lianliu55 <lianliu55@gmail.com>
2024-11-24 20:53:47 +0000

    Basic activity 1 & 2

---
+++ basic.js
@@ -0,0 +1,13 @@
+const a = -5 + 8*6 //declare a variables
+console.log(a) //undefined
+const b = (55+9)%9 //declare b variables
+console.log(b); //undefined
+const c = 20 //undefined
+const d = -3*5/8 //undefined
+console.log(a+b); //undefined
+const e = 5 //undefined
+const f = 15/3*2 //undefined
+const g = 8%3 //undefined
+console.log(e+f-g);//undefined
+
+
