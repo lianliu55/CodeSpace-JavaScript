bbdb44b85f24f3bb505f2036f9327d6e3e8de9e6
lianliu55 <lianliu55@gmail.com>
2025-01-05 17:57:13 +0000

    add README.md and organising the files

---
+++ README.md
@@ -0,0 +1,3 @@
+## Codespace Javascript
+
+A file structure for Javascript projects, notes and learnings
---
+++ scripts/basics_1.js
@@ -0,0 +1,29 @@
+/* Write a JavaScript program to print the result of the following operations.
+
+Test Data:
+a. -5 + 8 * 6 
+b. (55+9) % 9 
+c. 20 + -3*5 / 8 
+d. 5 + 15 / 3 * 2 - 8 % 3
+*/
+
+// Example A
+const a = -5 + 8*6
+console.log(a)
+
+// Example B
+const b = (55+9) % 9
+console.log(b);
+
+// Example C
+const c = 20
+const d = -3*5 / 8
+console.log(c+d);
+
+// Example D
+const e = 5
+const f = 15 / 3*2
+const g = 8%3
+console.log(e+f-g);
+
+
